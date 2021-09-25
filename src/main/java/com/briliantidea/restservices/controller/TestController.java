38cbb746e9e60c52b9cb70d6cd2b1ba887736af6
BahaminSH <bahamin.shahsavand@gmail.com>
2021-09-25 23:08:47 +0330

    3 mehr 1400

---
+++ src/main/java/com/briliantidea/restservices/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.briliantidea.restservices.controller;
+
+
+import com.briliantidea.restservices.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @Autowired(required = false)
+    private TestService testService;
+
+    @RequestMapping(value = "/hellouser", method = RequestMethod.POST)
+    public String helloUser(@RequestBody String userName) {
+        String result = "Hello";
+        testService.test();
+        return result + "" + userName;
+    }
+}
